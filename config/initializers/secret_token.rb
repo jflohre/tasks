1a07a4a5b10e2124e40f263e6f92d13b0d2f3543
Jacob Flohre <jake@heysparkbox.com>
2013-05-02 10:57:30 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasks::Application.config.secret_token = '12aaeea98d90e2676f662e6a9e572f90f38bc4d96f617965f66d360d69241db33ea3af4e82a2f715f2d5423e48fb05f320b990ce7743623055fc2e1a8c142383'
